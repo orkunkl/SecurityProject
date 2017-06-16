ef699b05dd9f4229a56107a03d9f9c24015ee249
orkun <kulceorkunfun@gmail.com>
2017-06-16 06:55:48 +0200

    console log edit

--- ui/src/app/services/auth-guard.service.ts
+++ ui/src/app/services/auth-guard.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { CanActivate } from '@angular/router';
-// Import our authentication service
 import { AuthService } from './auth.service';
 
 @Injectable()
@@ -9,8 +8,6 @@ export class AuthGuardService implements CanActivate {
 
   constructor(private AuthService: AuthService, private router: Router) { }
   canActivate(): boolean {
-        // I'm assuming that your guard1.canActivate and guard2.canActivate return boolean
-        console.log(this.AuthService.loggedIn())
         if (this.AuthService.loggedIn()){
         	return true
         }
